d46126f52f3756454f9fda3718eeb5f65fde106c
Greg Pfaff <greg.pfaff@razorfish.com>
2014-06-05 21:56:53 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var galleryimages = document.querySelectorAll("#gallery figure");
+for(i=0; i<galleryimages.length; i++){
+  galleryimages[i].addEventListener( 'click', function(){
+    this.classList.toggle("expanded");
+    gallery.classList.toggle("full")}
+  );
+  }
+
